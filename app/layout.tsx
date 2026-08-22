310e16508ba234745e0f1770d9d2e1cd9f3a4dbb
mohsin sikder <mohsinsikder895@gmail.com>
2026-08-22 16:57:36 +0600

    google and fb tag manager update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { Providers } from "@/components/providers";
 import { CartProvider } from '@/lib/cart-context'
@@ -80,6 +80,9 @@ export const viewport: Viewport = {
     ],
 }
 
+const GTM_ID = 'GTM-554667ZP'
+const FB_PIXEL_ID = '539934775177061'
+
 export default function RootLayout({
                                        children,
                                    }: Readonly<{
@@ -87,11 +90,48 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className={`${geistSans.variable} ${geistMono.variable} bg-background`}>
+        <head>
+            <Script id="gtm-script" strategy="afterInteractive">
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');`}
+            </Script>
+            <Script id="fb-pixel" strategy="afterInteractive">
+                {`!function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${FB_PIXEL_ID}');
+                fbq('track', 'PageView');`}
+            </Script>
+        </head>
         <body className="font-sans antialiased">
+        <noscript>
+            <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+            />
+        </noscript>
+        <noscript>
+            <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                alt=""
+            />
+        </noscript>
         <Providers>
             <CartProvider>
                 {children}
-                {process.env.NODE_ENV === 'production' && <Analytics />}
             </CartProvider>
         </Providers>
         </body>
